7c6dc1637b463f91058dac648504f6902bf3fe0b
shaheen-senpai <shaheen50053@gmail.com>
2024-07-15 11:46:22 +0530

    chore: Update styles for employee list and not found pages

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
+import "../assets/styles/notFound/notfound.scss";
 
 const NotFound = () => {
 	return (
-		<div>
-			<h1>404 - Not Found!</h1>
-			<Link to="/">Go Home</Link>
-		</div>
+		<section className="not-found">
+			<div className="container">
+				<div className="hero">
+					<h1>404 Not Found</h1>
+					<div className="content-box">
+						<h2>Looks like you&apos;re lost</h2>
+						<p>The page you are looking for is not available!</p>
+						<Link to="/" className="home-link">
+							Go Home
+						</Link>
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
 
