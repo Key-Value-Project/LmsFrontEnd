798f0148f034535acae17c3fd9713e648500e550
curioSOwl <aparnaanand171@gmail.com>
2024-07-23 10:51:27 +0530

    added return

--- src/pages/BorrowDetails.jsx
+++ src/pages/BorrowDetails.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import BorrowCard from "../components/library/BorrowCard";
 import BorrowCardHead from "../components/library/BorrowCardHead";
 import { useGetBorrowHistoryQuery } from "../api/library/api.library";
+import { Link } from "react-router-dom";
 
 const BorrowDetails = () => {
   const { data: borrowedData } = useGetBorrowHistoryQuery();
@@ -17,15 +18,17 @@ const BorrowDetails = () => {
           <h1>Borrowed Books</h1>
           <div className="top-header-components"></div>
         </div>
-        <div className="book-list">
-          <BorrowCardHead />
-          <div className="employee-list">
-            {borrowedData &&
-              borrowedData.map((book, index) => (
-                <BorrowCard key={index} {...book} />
-              ))}
+       
+          <div className="book-list">
+            <BorrowCardHead />
+            <div className="employee-list">
+              {borrowedData &&
+                borrowedData.map((book, index) => (
+                  <BorrowCard key={index} {...book} />
+                ))}
+            </div>
           </div>
-        </div>
+    
       </div>
     </>
   );
