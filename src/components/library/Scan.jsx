eafdbdcb9724ca1e5f495518e96dedeb9bfafd28
joeljoby111 <joeljoby111@gmail.com>
2024-07-23 16:38:06 +0530

    feat: borrow scan

--- src/components/library/Scan.jsx
+++ src/components/library/Scan.jsx
@@ -1,16 +1,43 @@
+import { useState } from 'react';
+import { useBorrowBookMutation, useGetAllShelvesQuery } from '../../api/library/api.library';
 import scan from '../../assets/icons/scan.svg';
 
 const Scan = () => {
+  const { data } = useGetAllShelvesQuery();
+  const [isbn, setIsbn] = useState('');
+  const [shelf, setShelf] = useState('');
+  const [borrow] = useBorrowBookMutation();
+  const handleClick = async () => {
+    const checkborrow = await borrow({ isbn: parseInt(isbn), shelf_id: shelf });
+    console.log(checkborrow);
+  };
+
   return (
     <div className="scan__form">
       <div className="icon">
         <img src={scan} />
       </div>
       <div className="scan__input">
-        <input type="text" placeholder="Enter ISBN" />
-        <input type="text" placeholder="Enter Shelf no." />
+        <input type="number" placeholder="Enter ISBN" value={isbn} onChange={(e) => setIsbn(e.target.value)} />
+        <select value={shelf} onChange={(e) => setShelf(e.target.value)}>
+          <option value="">Select Shelf</option>
+          {data &&
+            data.map((shelf, index) => (
+              <option key={index} value={shelf.id}>
+                {shelf.code}
+              </option>
+            ))}
+        </select>
       </div>
-      <button className="btn">Submit</button>
+      <button
+        className="btn"
+        onClick={(e) => {
+          e.preventDefault();
+          handleClick();
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 };
