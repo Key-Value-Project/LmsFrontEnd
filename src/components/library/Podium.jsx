5fb485c085886d1921d86362a9b517530f136af7
joeljoby111 <joeljoby111@gmail.com>
2024-07-25 02:03:03 +0530

    refactor: update class name in Podium component

--- src/components/library/Podium.jsx
+++ src/components/library/Podium.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Podium = ({ data }) => {
   return (
-    <div className='podium'>
+    <div className="podium">
       <div className="podium4"></div>
       <div className="podium2"></div>
       <div className="podium1"></div>
