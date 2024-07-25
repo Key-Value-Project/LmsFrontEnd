b9f987bdfc4cd5c9f4c17153c554563c8faeed90
Shaheen <84270559+shaheen-senpai@users.noreply.github.com>
2024-07-25 12:40:54 +0530

    Review integration (#40)
    * chore: review api added
    
    * refactor: optimize code for fetching and displaying book reviews
    
    * refactor: optimize code for displaying book reviews and add scrollable ratings list

--- src/components/library/ReviewsPanel.jsx
+++ src/components/library/ReviewsPanel.jsx
@@ -4,23 +4,27 @@ const ReviewsPanel = ({ reviews }) => {
   console.log(reviews);
   console.log(Array.from({ length: reviews[0].rating }));
   return (
-    <div className="rev_cont" id="rev">
-      <div className="book__det">
+    <div className="rev_cont" id="rev" style={{ padding: '30px' }}>
+      <div className="book__dett">
         <h2 className="review__head">Reviews</h2>
-        {reviews.map((review) => (
-          <div key={review.id} className="review-content">
-            <div className="each_rev">
-              <div>{review.employee.name}</div>
-              {/* <img src={starIcon} alt="star" /> */}
-              <div>
-                {Array.from({ length: review.rating }).map((something, i) => (
-                  <img key={i} src={starIcon} alt="star" />
-                ))}
+        <div className="ratings_list">
+          {reviews.map((review) => (
+            <div key={review.id} className="reviewcontent">
+              <div className="each_rev">
+                <div>
+                  <p>{review.employee.name}</p>
+                </div>
+                {/* <img src={starIcon} alt="star" /> */}
+                <div style={{ display: 'Flex', gap: '5px' }}>
+                  {Array.from({ length: review.rating }).map((something, i) => (
+                    <img key={i} src={starIcon} alt="star" />
+                  ))}
+                </div>
               </div>
+              <div>{review.comment}</div>
             </div>
-            <div>{review.comment}</div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
